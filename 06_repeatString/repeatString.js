b0f12c1fc9b8024279696eba4dd4c95d24bfb286
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-07-08 21:21:35 +0800

    Completed exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, num) {
+    let value = "";
+    let i = 0;
+    while (i < num) {
+        value += string;
+        i++;
+    }
+    return value;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
