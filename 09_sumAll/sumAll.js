f6122edfa3d7f57145e41d4e6b94735393eed58c
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-07-08 22:38:08 +0800

    Completed exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
