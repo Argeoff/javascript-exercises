c9896a455636bd9c8436799b414df6584578d273
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-07-08 22:14:13 +0800

    Completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let phrase = "";
+    let index = -1;
+    for (let i = 0; i < input.length; i++){
+        phrase += input.slice(index, index+1);
+        index--;
+    }
+    return phrase;
 };
 
 // Do not edit below this line
