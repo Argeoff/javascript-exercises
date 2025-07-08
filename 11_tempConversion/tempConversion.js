a888a184f763f68c0599d980cf47ba60042dba4a
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-07-09 00:01:57 +0800

    Completed exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cel) {
+  const result = (cel - 32) * 5/9;
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fan) {
+  const result = (fan * 9/5) + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
